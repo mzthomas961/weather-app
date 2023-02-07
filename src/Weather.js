963ae0e0c600b89a12cdbeb1496feb9910d31664
matt <matt@pop-os.localdomain>
2023-02-07 17:09:36 -0500

    added key

--- src/Weather.js
+++ src/Weather.js
@@ -6,7 +6,7 @@ const Weather = () => {
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(async (position) => {
       const { latitude, longitude } = position.coords;
-      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=YOUR_API_KEY`);
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=2f0e069ac3ef5d3a32d0ab39b498133c&units=imperial`);
       const data = await response.json();
       setWeather(data);
     });
@@ -15,6 +15,7 @@ const Weather = () => {
   if (!weather.main) {
     return <div>Loading...</div>;
   }
+  console.log(weather)
 
   return (
     <div>
